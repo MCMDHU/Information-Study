92c16479a95bebd46b065bded0a9c58319654ab0
WLX <annawlx1012@gmail.com>
2018-01-18 16:11:03 +0800

    repository changing

--- repository/十大算法/图论/哈密尔顿回路/TSP模拟退火/accept.m
+++
@@ -1,2 +0,0 @@
-function y=accept(t,df)
-y=(df<0)|(((df/t)<88)&(exp(-df/t)>rand(1,1)));
--- repository/十大算法/遗传算法/与已有的相同/xcross.m
+++
@@ -1,4 +0,0 @@
-function  [newpop ,len]=xcross(child,bounds,CP)
- mychild=child(:,1:end-1);
-[B(1,:),len]=B2F(mychild(1,:),bounds);
-newpop=B(1,:);
